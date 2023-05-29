95295e41b4cad4b97681844052dfa16c72382d88
TasnimAnjum <114102739+TasnimAnjum@users.noreply.github.com>
2023-05-29 16:54:59 +0600

    show list problem

---
+++ DataAnotation/Controllers/EmployeeController.cs
@@ -0,0 +1,31 @@
+﻿using DataAnotation.Context;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace DataAnotation.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        EmployeeContext db = new EmployeeContext();
+        // GET: Employee
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View(db.Employees.ToList());
+        }
+
+        [HttpGet]
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+       
+
+    }
+}
---
+++ DataAnotation/Models/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DataAnotation.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        [DisplayName("Employee name")]
+        [Required(ErrorMessage ="Employee name  is required")]
+        [StringLength(50)]
+        public string Name { get; set; }    
+
+        public string Address { get; set; }
+
+        public decimal Salary { get; set; }
+
+        public string Email { get; set; }
+    }
+}
